6ec7b221fa0ecedd89fdb25e6dc7fadd804d4e91
ordepoaoj123 <joaopedrocostasantosmelo10@gmail.com>
2026-02-27 22:07:25 -0300

    a7

---
+++ T.E/scripts.js
@@ -0,0 +1,9 @@
+let botao = document.querySelector("button");
+console.log(botao);
+
+function criar(){
+    alert("chamei a função");
+}
+botao.addEventListener("click", criar);
+let textoUsuario= document.querySelector("textarea");
+console.log(textoUsuario.value);
